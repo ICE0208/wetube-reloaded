01776a2a3f88ff948930da1e8c383da831393f67
ICE0208 <doky3307@naver.com>
2022-06-21 22:15:27 +0900

    middleware

--- src/server.js
+++ src/server.js
@@ -4,15 +4,39 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue.");
+  next();
+};
+
 const handleHome = (req, res) => {
-  return res.send("Main Page");
+  return res.send("<h1>Welcome</h1>");
 };
 const handleLogin = (req, res) => {
   return res.send("Login here.");
 };
+
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge.");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
-const handleListening = () => console.log(`Server listening on port ${PORT}`);
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(4000, handleListening);
