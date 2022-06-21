c93168338600b063a60c51133618b13a2f58d620
ICE0208 <doky3307@naver.com>
2022-06-21 19:02:55 +0900

    Request & Response

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
-import express from "express";
+import express, { response } from "express";
 
 const PORT = 4000;
 
 const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("Main Page");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login here.");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`Server listening on port ${PORT}`);
 
