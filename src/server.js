21b9d2c17a4441996bcc9169765a4a996d1d1581
ICE0208 <doky3307@naver.com>
2022-06-25 15:50:05 +0900

    Routers

--- src/server.js
+++ src/server.js
@@ -1,21 +1,18 @@
 import express, { response } from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  return res.send("<h1>Hello</h1>");
-};
-const login = (req, res) => {
-  return res.send("<h1>login<h1>");
-};
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
