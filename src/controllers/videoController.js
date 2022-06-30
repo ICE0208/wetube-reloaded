a416dfd8224439aefac729e509cca63662ad59a4
ICE0208 <doky3307@naver.com>
2022-06-30 23:32:33 +0900

    Array Database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,37 +1,39 @@
+let videos = [
+  {
+    title: "First video",
+    rating: 5,
+    comments: 1,
+    createdAt: "2 minutes ago",
+    views: 4,
+    id: 1,
+  },
+  {
+    title: "Second video",
+    rating: 4,
+    comments: 44,
+    createdAt: "7 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third video",
+    rating: 4,
+    comments: 23,
+    createdAt: "25 minutes ago",
+    views: 543,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
