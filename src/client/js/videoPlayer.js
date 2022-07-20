64540259f52c05721209ea3d56e9e4b40e6782f3
ICE0208 <doky3307@naver.com>
2022-07-20 22:49:37 +0900

    Mute & Ummute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,7 +4,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -21,9 +21,25 @@ const handlePlay = () => {
   playBtn.innerText = "Pause";
 };
 
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+};
+
+const handleVolume = (e) => {
+  if (video.muted) {
+    muteBtn.innerText = "Unmute";
+    volumeRange.value = 0;
+  } else {
+    muteBtn.innerText = "Mute";
+  }
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 video.addEventListener("pause", handlePasue);
 video.addEventListener("play", handlePlay);
+video.addEventListener("volumechange", handleVolume);
