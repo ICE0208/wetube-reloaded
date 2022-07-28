3e6a9ce54f405f7168ac0080065ca663c2b9487c
ICE0208 <doky3307@naver.com>
2022-07-28 15:55:09 +0900

    Comment Models

--- src/models/User.js
+++ src/models/User.js
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
   location: String,
 });
