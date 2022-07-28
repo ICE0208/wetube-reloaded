bc63b521e210a7e33f33c0f5c323319cefc21fdd
ICE0208 <doky3307@naver.com>
2022-07-28 17:08:33 +0900

    API Route

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -4,8 +4,19 @@ const form = document.getElementById("commentForm");
 const handleSubmit = (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
-  const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const text = textarea.value.trim();
+  const videoId = videoContainer.dataset.id;
+  if (text === "") return;
+
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      text,
+    }),
+  });
 };
 
 if (form) {
