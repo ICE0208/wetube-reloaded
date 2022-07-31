46e32f09799edd6bb7cd64b79f7f073a5a7fd5ec
ICE0208 <doky3307@naver.com>
2022-07-31 18:29:30 +0900

    Deployment

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
     commentSection: BASE_JS + "commentSection.js",
     deleteVideo: BASE_JS + "deleteVideo.js",
   },
-  mode: "development",
-  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
